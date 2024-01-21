c1f417a5489a8b04d7c9f7e4a8832f0eb4649e69
connoryim <0134269333+connoryim@users.noreply.github.com>
2024-01-21 03:46:24 -0500

    Add psuedo code to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//A function that when called creates a new object that stores information
+//on a given book
+
+//A funtion that when called takes a book object and creates a new block
+//on screen displaying the book information
+
+//A function that when called deletes selected book object. Connected to
+// a button that is dispalyed on each individual book block on screen
+
+//A function that when called displays a form for the user to input
+// information on a book
