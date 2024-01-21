c02229fec5f4d322dfb09cd27e4e8507dada34e5
connoryim <0134269333+connoryim@users.noreply.github.com>
2024-01-21 04:38:45 -0500

    Add funtion that creates a form when called

--- script.js
+++ script.js
@@ -9,3 +9,34 @@
 
 //A function that when called displays a form for the user to input
 // information on a book
+function addBook () {
+    var form = document.createElement("form");
+
+    var title = document.createElement("input");
+    title.type = "text";
+    title.name = "Title";
+    title.placeholder = "title";
+
+    var author = document.createElement("input");
+    author.type = "text";
+    author.name = "Author";
+    author.placeholder = "author";
+
+    var pages = document.createElement("input");
+    pages.type = "number";
+    pages.name = "pages";
+    pages.placeholder = "pages";
+
+    var read = document.createElement("input");
+    read.type = "checkbox";
+    read.name = "read"
+
+    form.appendChild(title);
+    form.appendChild(author);
+    form.appendChild(pages);
+    form.appendChild(read);
+
+    document.body.appendChild(form);
+}
+
+addBook();
